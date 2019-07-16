dee3cfb1d4cec44368d0864af60b4eb212694baa
Jean Fellipe Ferraz Campos <jeanffc@gmail.com>
2019-07-15 21:44:59 -0300

    feat: added paths to users/ and users/pending_approval

--- server.js
+++ server.js
@@ -7,10 +7,19 @@ app.use(favicon(__dirname + "/build/favicon.ico"));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function(req, res) {
-  return res.send("pong");
-});
+
 app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+
+app.get("/users/pending_approval", function(req, res) {
+  
+});
+
+app.post("/users/", function(req, res) {
+  
+});
+
 app.listen(port);
+
+console.log(`http://localhost:${port}`);
